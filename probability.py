1e245e9611de5d4b4c64cb68b8228264369053f2
vietjtnguyen <viet@vietjtnguyen.com>
2013-06-28 01:48:09 -0700

    Auto-save 2013-06-28T01-48-09

--- probability.py
+++ probability.py
@@ -358,12 +358,12 @@ class BayesianNetwork():
 				product *= conditional.context_tables[assignment.project(self.families[variable])].probabilities[assignment.project([variable])]
 			joint_table.probabilities[assignment] = product
 		return joint_table
-	def get_display_html(self, width=640, height=480):
-		return 'var links=[{:}];'.format(''.join(['{{source:"{:}",target:"{:}"}},'.format(edge.from_var, edge.to_var) for edge in self.edges]))
+	def get_display_js(self, width=640, height=480):
+		return 'var links=[{:}];alert("{:}");'.format(''.join(['{{source:"{:}",target:"{:}"}},'.format(edge.from_var, edge.to_var) for edge in self.edges]))
 	def display(self, width=640, height=480):
 		import IPython.display
-		IPython.display.display(IPython.display.Javascript(data=self.get_display_html(width, height)))
-		IPython.display.display(IPython.display.Javascript(filename='graph_display.js', lib='http://d3js.org/d3.v3.min.js', css='graph_display.css'))
+		IPython.display.display(IPython.display.Javascript(data=self.get_display_js(width, height)))
+		#IPython.display.display(IPython.display.Javascript(filename='graph_display.js', lib='http://d3js.org/d3.v3.min.js', css='graph_display.css'))
 
 S, H, E = variables = map(Variable, ['S', 'H', 'E'])
 h, h_ = H.get_assignments()

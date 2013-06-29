f89af17303c0336b4ffefade723b5e69c407a41f
vietjtnguyen <viet@vietjtnguyen.com>
2013-06-28 22:55:40 -0700

    Auto-save 2013-06-28T22-55-40

--- discrete_probability/__init__.py
+++ discrete_probability/__init__.py
@@ -1,2 +1,6 @@
+from graph import DirectedEdge
 from tables import JointTable, ConditionalTable
 from variables import Variable, SingleAssignment, Assignment
+import bayesnet
+import infotheory
+
---
+++ discrete_probability/graphical/__init__.py
@@ -0,0 +1,34 @@
+import collections
+import math
+import random
+import sys
+
+class DirectedEdge():
+	def __init__(self, from_var, to_var, right=True):
+		self.from_var = from_var
+		self.to_var = to_var
+		self.right = right
+	def __str__(self):
+		return '{:} > {:}'.format(str(self.from_var), str(self.to_var))
+	def __repr__(self):
+		return str(self)
+
+#def gather_families(self):
+#	self.root_variables = set()
+#	self.families = {}
+#	for variable in self.variables:
+#		parents = []
+#		for edge in self.edges:
+#			if edge.to_var == variable:
+#				parents.append(edge.from_var)
+#		self.families[variable] = set(parents)
+#		if len(parents) == 0:
+#			self.root_variables.add(variable)
+#	self.parental_variables = set()
+#	for edge in self.edges:
+#		self.parental_variables.add(edge.from_var)
+#	self.leaf_variables = set(self.variables).difference(self.parental_variables)
+#	self.conditionals = {}
+#	for variable in self.variables:
+#		self.conditionals[variable] = ConditionalTable([variable], self.families[variable])
+
